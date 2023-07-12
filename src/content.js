441557f96d68a2f024c4c9ddaf39f77472c4836c
Philip7489 <2462160c@student.gla.ac.uk>
2023-07-12 11:43:00 +0100

    CV fix

--- src/content.js
+++ src/content.js
@@ -17,14 +17,14 @@ function content() {
             
                 <p className="textP">For more details see my CV below.</p>
 
-                <form action='http://localhost:3000/CV.pdf' target="_blank">
+                <form action='http://www.philipcoffey.co.uk/CV.pdf' target="_blank">
                     <input id="CVInput" type="submit" value="View my CV" />
                 </form>
             </div>
 
             <div id="projectsDiv">
                 <SubTitle text={"Projects"}/>
-                <Project title={"Military Grid Reference System"} image={MGRSImg} link={"https://gla-my.sharepoint.com/personal/2461352f_student_gla_ac_uk/_layouts/15/stream.aspx?id=%2Fpersonal%2F2461352f%5Fstudent%5Fgla%5Fac%5Fuk%2FDocuments%2FMicrosoft%20Teams%20Chat%20Files%2FCS16%20Demo%20%281%29%2Emp4&referrer=Teams%2ETEAMS%2DELECTRON&referrerScenario=p2p%5Fns%2Dbim&ga=1"} text={"This Military Grid Reference System comes from a third year project where me and three other students worked with a customer in an agile team setting. Our customer was aerospace company Thales. This project taught me a great deal on working effectively within a team. My main contributions to this app where implementing the 'Shape drawing', the 'map overlays' and acting as 'chair' for out customer meetings."}/>
+                <Project title={"Military Grid Reference System"} image={MGRSImg} link={"https://www.youtube.com/watch?v=xKLrNE7hxhU&ab_channel=PhilipCoffey"} text={"This Military Grid Reference System comes from a third year project where me and three other students worked with a customer in an agile team setting. Our customer was aerospace company Thales. This project taught me a great deal on working effectively within a team. My main contributions to this app where implementing the 'Shape drawing', the 'map overlays' and acting as 'chair' for out customer meetings."}/>
                 <Project title={"Resolvable Block design Constructor"} image={RBDCImg} link={"https://resolvable-bd-constructor.herokuapp.com/"} text={"This application was made as part of my forth year individual project. While the most significant challenge of this project was understanding the relevant math, building the app still required me to improve my development which I think is evident when comparing with my MGRS web application. This application was also my first major project built with django and my first time hosting a project online."}/>
                 <Project title={"Sudoku React App"} image={sudokuImg} link={"https://sudoku-react-application-95df4e0d2f6d.herokuapp.com/"} text={"I made this sudoku web app for the purpose of teaching myself how to use react. Throughout the development of this project I learned a lot about making a flexible front end which can maintain it's layout throughout different devices and screen sizes. I refactored the aesthetic of this application multiple times as I learned about making a consistent theme by utilizing complementary colours."}/>
             </div>
