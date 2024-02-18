55204fe69a45ad55faa9ae2c10f5c8a9bf0ac5b7
Philip7489 <2462160c@student.gla.ac.uk>
2024-02-18 21:03:45 +0000

    fds

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
-import './Project.css'
+import './Project.css';
+import leftArrow from "./images/leftArrow.png";
+import rightArrow from "./images/rightArrow.png";
 
 function Project({title, text, image, link, leftImage}) {
     function openLink(){
@@ -12,7 +14,13 @@ function Project({title, text, image, link, leftImage}) {
                 {title}
             </div>
             <div id="imageDiv">
+                <div id="leftArrow">
+                    <img id="leftArrow" src={leftArrow}/>
+                </div>
                 <img id="image" src={image}/>
+                <div id="leftArrow">
+                    <img id="rightArrow" src={rightArrow}/>
+                </div>
             </div>
             <div id="textDiv">
                 {text}
